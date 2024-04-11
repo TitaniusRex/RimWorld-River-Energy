a63d09866d8d7664b0af19797392ae7e8e72586b
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-04-11 04:09:00 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -17,6 +17,7 @@ public class CompPowerPlantWPGTWater : CompPowerPlant
 	private const float BladeOffset = 1f;
 	private const int BladeCount = 12;
 	public static readonly Material BladesMat = MaterialPool.MatFrom("Things/Building/Power/WaterPowerGenerator/HydroelectricPowerStationBlades");
+ 
 	protected override float DesiredPowerOutput
 	{
 		get
@@ -57,4 +58,51 @@ public class CompPowerPlantWPGTWater : CompPowerPlant
 	private void ClearCache()
 	{
 		cacheDirty = true;
+	}
+
+ 	private void RebuildCache()
+	{
+		waterUsable = true;
+		foreach (IntVec3 item in WaterCells())
+		{
+			if (item.InBounds(parent.Map) && !parent.Map.terrainGrid.TerrainAt(item).affordances.Contains(TerrainAffordanceDefOf.MovingFluid))
+			{
+				waterUsable = false;
+				break;
+			}
+		}
+		waterDoubleUsed = false;
+		IEnumerable<Building> enumerable = parent.Map.listerBuildings.AllBuildingsColonistOfDef(ThingDefOf.WWPGTHydroelectricPowerStation);
+		foreach (IntVec3 item2 in WaterUseCells())
+		{
+			if (!item2.InBounds(parent.Map))
+			{
+				continue;
+			}
+			foreach (Building item3 in enumerable list)
+			{
+				if (item3 != parent && item3.GetComp<CompPowerPlantWPGTWater>().WaterUseRect().Contains(item2))
+				{
+					waterDoubleUsed = true;
+					break;
+				}
+			}
+		}
+		if (!waterUsable)
+		{
+			spinRate = 0f;
+			return;
+		}
+		Vector3 zero = Vector3.zero;
+		foreach (IntVec3 item4 in WaterCells())
+		{
+			zero += parent.Map.waterInfo.GetWaterMovement(item4.ToVector3Shifted());
+		}
+		spinRate = Mathf.Sign(Vector3.Dot(zero, parent.Rotation.Rotated(RotationDirection.Clockwise).FacingCell.ToVector3()));
+		spinRate *= Rand.RangeSeeded(2.4f, 2.6f, parent.thingIDNumber * 60509 + 33151);
+		if (waterDoubleUsed)
+		{
+			spinRate *= 0.5f;
+		}
+		cacheDirty = false;
 	}
