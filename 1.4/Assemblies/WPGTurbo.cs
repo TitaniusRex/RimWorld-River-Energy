e982e83ddcbfd0aa96fbb7352e0a16f0ac700a0d
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-04-13 22:38:20 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -150,5 +150,17 @@ public IEnumerable<IntVec3> WaterCells()
 	}
 
  
+	public IEnumerable<IntVec3> WaterUseCells()
+	{
+		return WaterUseCells(parent.Position, parent.Rotation);
+	}
+
+	public static IEnumerable<IntVec3> WaterUseCells(IntVec3 loc, Rot4 rot)
+	{
+		foreach (IntVec3 item in WaterUseRect(loc, rot))
+		{
+			yield return item;
+		}
+	}
 
  
