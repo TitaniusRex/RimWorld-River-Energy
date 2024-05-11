398f3776e92130ed7e47ad0bcc3af29be42009d9
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-05-11 03:14:12 +0300

    Update WPGTurbo.cs

--- 1.5/Assemblies/WPGTurbo.cs
+++ 1.5/Assemblies/WPGTurbo.cs
@@ -58,3 +58,13 @@ public class CompPowerPlantWPGTWaterSmoll : CompPowerPlant
 		ForceOthersToRebuildCache(parent.Map);
 	}
 
+public override void PostDeSpawn(Map map)
+	{
+		base.PostDeSpawn(map);
+		ForceOthersToRebuildCache(map);
+	}
+
+	private void ClearCache()
+	{
+		cacheDirty = true;
+	}
