90967ec968a8042ba968010f9fa5e570ec5de0db
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-05-11 03:16:16 +0300

    Update WPGTurbo.cs

--- 1.5/Assemblies/WPGTurbo.cs
+++ 1.5/Assemblies/WPGTurbo.cs
@@ -129,3 +129,14 @@ public override void PostDeSpawn(Map map)
 	{
 		return WaterCells(parent.Position, parent.Rotation);
 	}
+
+
+	public static IEnumerable<IntVec3> WaterCells(IntVec3 loc, Rot4 rot)
+	{
+		IntVec3 perpOffset = rot.Rotated(RotationDirection.Counterclockwise).FacingCell;
+		yield return loc + rot.FacingCell * 3;
+		yield return loc + rot.FacingCell * 3 - perpOffset;
+		yield return loc + rot.FacingCell * 3 - perpOffset * 2;
+		yield return loc + rot.FacingCell * 3 + perpOffset;
+		yield return loc + rot.FacingCell * 3 + perpOffset * 2;
+	}
