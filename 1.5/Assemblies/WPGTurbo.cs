bc1ccfd9afbdcad01eb1ce9ce234e145e77eb762
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-05-15 02:02:39 +0300

    Update WPGTurbo.cs

--- 1.5/Assemblies/WPGTurbo.cs
+++ 1.5/Assemblies/WPGTurbo.cs
@@ -172,6 +172,7 @@ public static CellRect WaterUseRect(IntVec3 loc, Rot4 rot)
  public static IEnumerable<IntVec3> GroundCells(IntVec3 loc, Rot4 rot)
 	{
 		IntVec3 perpOffset = rot.Rotated(RotationDirection.Counterclockwise).FacingCell;
+		yield return loc - rot.FacingCell;
 		
 	}
  
