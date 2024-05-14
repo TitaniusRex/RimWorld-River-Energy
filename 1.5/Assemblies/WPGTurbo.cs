a1793205a75633ba1470104217f143bb4fcebdd5
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-05-15 02:03:39 +0300

    Update WPGTurbo.cs

--- 1.5/Assemblies/WPGTurbo.cs
+++ 1.5/Assemblies/WPGTurbo.cs
@@ -175,6 +175,7 @@ public static CellRect WaterUseRect(IntVec3 loc, Rot4 rot)
 		yield return loc - rot.FacingCell;
 		yield return loc - rot.FacingCell - perpOffset;
 		yield return loc - rot.FacingCell + perpOffset;
+		yield return loc;
 		
 	}
  
