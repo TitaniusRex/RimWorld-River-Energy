99dd5f47ad0f19802365c3d7e234b0333739b45b
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-05-31 04:15:22 +0300

    Update WPGTurbo.cs

--- 1.5/Assemblies/WPGTurbo.cs
+++ 1.5/Assemblies/WPGTurbo.cs
@@ -153,9 +153,9 @@ using Verse;
 		    yield return item;
 		}
 	}
- public IEnumerable<IntVec3> GroundCells()
+ 	public IEnumerable<IntVec3> GroundCells()
 	{
-		return GroundCells(parent.Position, parent.Rotation);
+	    return GroundCells(parent.Position, parent.Rotation);
 	}
  public static IEnumerable<IntVec3> GroundCells(IntVec3 loc, Rot4 rot)
 	{
