a16301aab5b6d5a4861bdb870ece08981db5b47b
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-06-08 03:02:06 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -76,9 +76,9 @@ public class CompPowerPlantWPGTWater : CompPowerPlant
 	    foreach (IntVec3 item2 in WaterUseCells())
 	    {
 		if (!item2.InBounds(parent.Map))
-			{
-				continue;
-			}
+		{
+		     continue;
+		}
 			foreach (Building item3 in enumerable)
 			{
 				if (item3 != parent && item3.GetComp<CompPowerPlantWPGTWater>().WaterUseRect().Contains(item2))
