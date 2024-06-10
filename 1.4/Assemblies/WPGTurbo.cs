bdc409a3754cbd426df7a58dadbdee812beccf9f
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-06-10 03:07:31 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -130,7 +130,7 @@ public class CompPowerPlantWPGTWater : CompPowerPlant
 
 	public static IEnumerable<IntVec3> WaterCells(IntVec3 loc, Rot4 rot)
 	{
-		IntVec3 perpOffset = rot.Rotated(RotationDirection.Counterclockwise).FacingCell;
+	    IntVec3 perpOffset = rot.Rotated(RotationDirection.Counterclockwise).FacingCell;
 		yield return loc + rot.FacingCell * 3;
 		yield return loc + rot.FacingCell * 3 - perpOffset;
 		yield return loc + rot.FacingCell * 3 - perpOffset * 2;
