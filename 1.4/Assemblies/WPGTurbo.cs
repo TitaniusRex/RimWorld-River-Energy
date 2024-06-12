aa0279d770bfc138d7de65735a003d2507449e93
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-06-12 21:17:41 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -134,7 +134,7 @@ public class CompPowerPlantWPGTWater : CompPowerPlant
 	    yield return loc + rot.FacingCell * 3;
 	    yield return loc + rot.FacingCell * 3 - perpOffset;
 	    yield return loc + rot.FacingCell * 3 - perpOffset * 2;
-		yield return loc + rot.FacingCell * 3 + perpOffset;
+	    yield return loc + rot.FacingCell * 3 + perpOffset;
 		yield return loc + rot.FacingCell * 3 + perpOffset * 2;
 	}
  	public CellRect WaterUseRect()
