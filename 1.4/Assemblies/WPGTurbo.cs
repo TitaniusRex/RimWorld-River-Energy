6c03ab9a6c52b054323a9ef161aa142cf3a30ef0
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-06-22 14:11:36 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -190,7 +190,7 @@ public class CompPowerPlantWPGTWater : CompPowerPlant
 		float num = spinPosition + (float)Math.PI * 2f * (float)i / 9f;
 		float x = Mathf.Abs(4f * Mathf.Sin(num));
 		bool num2 = num % ((float)Math.PI * 2f) < (float)Math.PI;
-			Vector2 vector2 = new Vector2(x, 1f);
+		Vector2 vector2 = new Vector2(x, 1f);
 			Vector3 s = new Vector3(vector2.x, 1f, vector2.y);
 			Matrix4x4 matrix = default(Matrix4x4);
 			matrix.SetTRS(vector + Vector3.up * (3f / 74f) * Mathf.Cos(num), parent.Rotation.AsQuat, s);
