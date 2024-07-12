179f4df73ee607edbda90f944b4c842e3ca3a650
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-07-12 16:35:58 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -27,7 +27,7 @@ using Verse;
 		if (!waterUsable)
 		{ return 0f; }
 		if (waterDoubleUsed)
-		{return base.DesiredPowerOutput * 0.3f;}
+		{return base.DesiredPowerOutput * 0.3f; }
 		return base.DesiredPowerOutput;
 	    }
 	}
