3dc893042622e6016caf6e1ff979fb04da020ac7
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-07-20 03:01:55 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -201,7 +201,7 @@ using Verse;
 	private bool cacheDirty = true;
 	private bool waterUsable;
 	private bool waterDoubleUsed;
-	    private float spinRate = 1f;
+	private float spinRate = 1f;
 	    private const float PowerFactorIfWaterDoubleUsed = 0.3f;
 	    private const float SpinRateFactor = 1f / 100f;
             private const float BladeOffset = 1f;
