f4620c43eb2ae6c89c67382c62ca1cee7e8ee9bb
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-08-03 15:35:16 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -263,7 +263,7 @@ using Verse;
 		Vector3 zero = Vector3.zero;
 		foreach (IntVec3 item4 in WaterCells())
 		{
-			zero += parent.Map.waterInfo.GetWaterMovement(item4.ToVector3Shifted());
+		    zero += parent.Map.waterInfo.GetWaterMovement(item4.ToVector3Shifted());
 		}
 		spinRate = Mathf.Sign(Vector3.Dot(zero, parent.Rotation.Rotated(RotationDirection.Clockwise).FacingCell.ToVector3()));
 		spinRate *= Rand.RangeSeeded(0.4f, 0.6f, parent.thingIDNumber * 60509 + 33151);
