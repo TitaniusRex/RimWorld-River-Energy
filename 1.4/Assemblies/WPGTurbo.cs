83dfb3132638bb491e03c3bfc090b26bc03b9a35
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-08-03 15:33:30 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -251,7 +251,7 @@ using Verse;
 			if (item3 != parent && item3.GetComp<CompPowerPlantWPGTWater>().WaterUseRect().Contains(item2))
 			{
 			    waterDoubleUsed = true;
-			break;
+			    break;
 				}
 			}
 		}
