d22670c4092f8a5453c89ddad6dfbdb51752a154
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-08-13 02:56:50 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -264,7 +264,7 @@ using Verse;
 	    spinRate *= Rand.RangeSeeded(0.4f, 0.6f, parent.thingIDNumber * 60509 + 33151);
 	    if (waterDoubleUsed)
 	    {spinRate *= 0.5f; }
-		cacheDirty = false;
+	    cacheDirty = false;
 	}
 
 	private void ForceOthersToRebuildCache(Map map)
