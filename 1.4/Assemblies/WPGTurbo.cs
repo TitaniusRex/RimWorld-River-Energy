895caf8ca03c49727802ee20fca9780f3a02c3cf
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-08-15 11:20:25 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -285,7 +285,7 @@ using Verse;
 
 	public static IEnumerable<IntVec3> WaterCells(IntVec3 loc, Rot4 rot)
 	{
-		IntVec3 perpOffset = rot.Rotated(RotationDirection.Counterclockwise).FacingCell;
+	    IntVec3 perpOffset = rot.Rotated(RotationDirection.Counterclockwise).FacingCell;
 		yield return loc + rot.FacingCell * 3;
 		yield return loc + rot.FacingCell * 3 - perpOffset;
 		yield return loc + rot.FacingCell * 3 - perpOffset * 2;
