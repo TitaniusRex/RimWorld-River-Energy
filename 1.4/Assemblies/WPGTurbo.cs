a701fbef5a7e614dc086d589d5e7d49b28069778
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-08-17 19:48:25 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -300,7 +300,7 @@ using Verse;
         public static CellRect WaterUseRect(IntVec3 loc, Rot4 rot)
 	{
 	    int width = (rot.IsHorizontal ? 1 : 32);
-		int height = (rot.IsHorizontal ? 32 : 1);
+	    int height = (rot.IsHorizontal ? 32 : 1);
 		return CellRect.CenteredOn(loc + rot.FacingCell * 1, width, height);
 	}
  
