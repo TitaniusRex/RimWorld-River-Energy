cfb73866f48c90eb7eb1b90c1ddcaeceb64ba232
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-08-21 16:37:43 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -307,7 +307,7 @@ using Verse;
 	public IEnumerable<IntVec3> WaterUseCells()
 	{return WaterUseCells(parent.Position, parent.Rotation); }
  
-      public static IEnumerable<IntVec3> WaterUseCells(IntVec3 loc, Rot4 rot)
+        public static IEnumerable<IntVec3> WaterUseCells(IntVec3 loc, Rot4 rot)
 	{
 	    foreach (IntVec3 item in WaterUseRect(loc, rot))
 	    {yield return item; }
