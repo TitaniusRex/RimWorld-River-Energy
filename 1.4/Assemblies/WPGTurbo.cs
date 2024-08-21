290de65370551a7780dd4ad10ac3ec58329fcee1
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-08-21 16:36:57 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -328,7 +328,7 @@ using Verse;
 	    yield return loc - perpOffset;
 	    yield return loc + perpOffset;
 	    yield return loc + rot.FacingCell;
-		yield return loc + rot.FacingCell - perpOffset;
+	    yield return loc + rot.FacingCell - perpOffset;
 		yield return loc + rot.FacingCell + perpOffset;
 	}
 
