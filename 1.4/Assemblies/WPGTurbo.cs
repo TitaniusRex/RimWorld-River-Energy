b7fcdba818a40bd9ba5ddbeb9bd5fb55e44cc16e
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-08-23 16:20:16 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -337,7 +337,7 @@ using Verse;
 	    Vector3 vector = parent.TrueCenter();
 	    vector += parent.Rotation.FacingCell.ToVector3() * 0f;
 	    for (int i = 0; i < 9; i++)
-		{
+	    {
 			float num = spinPosition + (float)Math.PI * 2f * (float)i / 9f;
 			float x = Mathf.Abs(4f * Mathf.Sin(num));
 			bool num2 = num % ((float)Math.PI * 2f) < (float)Math.PI;
