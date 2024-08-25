ff9aac86f5b0d7ac7e68ad87a7a11bf23fc1860a
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-08-25 15:01:24 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -353,7 +353,7 @@ using Verse;
 	{
 	    string text = base.CompInspectStringExtra();
 	    if (waterUsable && waterDoubleUsed)
-		{
+	    {
 			text += "\n" + "Watermill_WaterUsedTwice".Translate();
 		}
 		return text;
