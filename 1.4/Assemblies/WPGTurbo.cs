81a8fa3b041b445cee67eb49e163f99dc22e4cae
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-08-25 15:01:03 +0300

    Update WPGTurbo.cs

--- 1.4/Assemblies/WPGTurbo.cs
+++ 1.4/Assemblies/WPGTurbo.cs
@@ -351,7 +351,7 @@ using Verse;
  
 	public override string CompInspectStringExtra()
 	{
-		string text = base.CompInspectStringExtra();
+	    string text = base.CompInspectStringExtra();
 		if (waterUsable && waterDoubleUsed)
 		{
 			text += "\n" + "Watermill_WaterUsedTwice".Translate();
